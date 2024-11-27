5584a37035c2167cde6fdcd774332f807e36cd1d
SMSzab <78252159+SMSzab@users.noreply.github.com>
2024-11-27 20:21:37 +0100

    Listview update on app

--- flutter_practice_basics/lib/main.dart
+++ flutter_practice_basics/lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(const MyApp());
 }
 
+class Post {
+  String body;
+  String author;
+  int likes = 0;
+  bool userLiked = false;
+
+  Post(this.body, this.author);
+
+  void likePost() {
+    this.userLiked = !this.userLiked;
+    if (this.userLiked) {
+      this.likes += 1;
+    } else {
+      this.likes -= 1;
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -29,11 +47,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String text = "";
+  List<Post> posts = [];
 
-  void changeText(String text) {
+  void newPost(String text) {
     this.setState(() {
-      this.text = text;
+      posts.add(new Post(text, "Som"));
     });
   }
 
@@ -42,8 +60,8 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar: AppBar(title: Text('Hi Brah!')),
         body: Column(children: <Widget>[
-          TextInputWidget(this.changeText),
-          Text(this.text)
+          Expanded(child: PostList(this.posts)),
+          TextInputWidget(this.newPost)
         ]));
   }
 }
@@ -68,6 +86,7 @@ class _TextInputWidgetState extends State<TextInputWidget> {
 
   void click() {
     widget.callback(controller.text);
+    FocusScope.of(context).unfocus();
     controller.clear();
   }
 
@@ -84,5 +103,53 @@ class _TextInputWidgetState extends State<TextInputWidget> {
               tooltip: "Post message",
               onPressed: this.click,
             )));
+  }
+}
+
+class PostList extends StatefulWidget {
+  final List<Post> listItems;
+
+  PostList(this.listItems);
+
+  @override
+  State<PostList> createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  void like(Function callback) {
+    this.setState(() {
+      callback();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (context, index) {
+        var post = this.widget.listItems[index];
+        return Card(
+            child: Row(children: <Widget>[
+          Expanded(
+              child: ListTile(
+            title: Text(post.body),
+            subtitle: Text(post.author),
+          )),
+          Row(
+            children: <Widget>[
+              Container(
+                child:
+                    Text(post.likes.toString(), style: TextStyle(fontSize: 20)),
+                padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+              ),
+              IconButton(
+                  icon: Icon(Icons.thumb_up),
+                  onPressed: () => this.like(post.likePost),
+                  color: post.userLiked ? Colors.purple : Colors.black)
+            ],
+          )
+        ]));
+      },
+    );
   }
 }
